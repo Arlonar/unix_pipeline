ac039dda75da7c5b6b1e31471aece907e10d9071
Arlonar <sarafannikov.artem@yandex.ru>
2024-06-05 22:48:53 +0300

    fix work time, all tests are ok

--- signer.go
+++ signer.go
@@ -2,109 +2,113 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
 )
 
+func getChannelCrcData(data string) chan string {
+	out := make(chan string, 1)
+	go func(output chan string) {
+		output <- DataSignerCrc32(data)
+	}(out)
+	return out
+}
+
 func SingleHash(in, out chan interface{}) {
-	hashFunc := func(val string) {
-		left := make(chan string)
-		right := make(chan string)
-
-		crCalc := func(value string, output chan string) {
-			output <- DataSignerCrc32(value)
-		}
-		mdCalc := func(value string, output chan string) {
-			output <- DataSignerMd5(value)
-		}
-
-		go crCalc(val, left)
-		go mdCalc(val, right)
+	wg := &sync.WaitGroup{}
+	hashFunc := func(val string, md5 string) {
+		defer wg.Done()
+		left := getChannelCrcData(val)
+		rValue := DataSignerCrc32(md5)
 		lValue := <-left
-		rValue := <-right
-
-		go crCalc(rValue, right)
-		rValue = <-right
-
 		out <- lValue + "~" + rValue
 	}
 
 	for data := range in {
+		wg.Add(1)
 		switch val := data.(type) {
 		case int:
-			hashFunc(strconv.Itoa(val))
+			str := strconv.Itoa(val)
+			md5 := DataSignerMd5(str)
+			go hashFunc(str, md5)
 		case string:
-			hashFunc(val)
+			wg.Add(1)
+			md5 := DataSignerMd5(val)
+			go hashFunc(val, md5)
 		default:
 			fmt.Println("Unknown type")
+			wg.Done()
 		}
 	}
+	wg.Wait()
 }
 
 const MultiHashTimes = 6
 
 func MultiHash(in, out chan interface{}) {
+	wgFunc := &sync.WaitGroup{}
 	for data := range in {
 		switch val := data.(type) {
 		case string:
-			hashes := make([]string, MultiHashTimes)
-			mx := &sync.Mutex{}
-			wg := &sync.WaitGroup{}
-			for i := 0; i < MultiHashTimes; i++ {
-				wg.Add(1)
-				go func(value string, index int, output []string) {
-					newHash := DataSignerCrc32(strconv.Itoa(index) + value)
-					mx.Lock()
-					output[index] = newHash
-					mx.Unlock()
-					wg.Done()
-				}(val, i, hashes)
-			}
-			wg.Wait()
+			wgFunc.Add(1)
+			go func(value string) {
+				defer wgFunc.Done()
+				hashes := make([]string, MultiHashTimes)
+				mx := &sync.Mutex{}
+				wgHash := &sync.WaitGroup{}
+				for i := 0; i < MultiHashTimes; i++ {
+					wgHash.Add(1)
+					go func(index int) {
+						newHash := DataSignerCrc32(strconv.Itoa(index) + value)
+						mx.Lock()
+						hashes[index] = newHash
+						mx.Unlock()
+						wgHash.Done()
+					}(i)
+				}
+				wgHash.Wait()
 
-			multiHash := strings.Builder{}
-			for i := 0; i < MultiHashTimes; i++ {
-				mx.Lock()
-				multiHash.WriteString(hashes[i])
-				mx.Unlock()
-			}
-			out <- multiHash.String()
+				multiHash := strings.Builder{}
+				for i := 0; i < MultiHashTimes; i++ {
+					mx.Lock()
+					multiHash.WriteString(hashes[i])
+					mx.Unlock()
+				}
+				out <- multiHash.String()
+			}(val)
 		}
 	}
+	wgFunc.Wait()
 }
 
 func CombineResults(in, out chan interface{}) {
-	builder := strings.Builder{}
+	var combinedStr []string
 	for value := range in {
 		switch str := value.(type) {
 		case string:
-			builder.WriteString(str)
-			builder.WriteString("_")
+			combinedStr = append(combinedStr, str)
 		}
 	}
-	temp := builder.String()
-	out <- temp[:len(temp)-1]
+	sort.Strings(combinedStr)
+	out <- strings.Join(combinedStr, "_")
 }
 
 func ExecutePipeline(workers ...job) {
 	in := make(chan interface{}, 2)
 	wg := &sync.WaitGroup{}
 
-	workFunc := func(f job, in chan interface{}) chan interface{} {
-		output := make(chan interface{})
-		go func() {
-			defer close(output)
-			defer wg.Done()
-			f(in, output)
-		}()
-		return output
-	}
-
 	for _, worker := range workers {
 		wg.Add(1)
-		in = workFunc(worker, in)
+		out := make(chan interface{})
+		go func(f job, workIn, workOut chan interface{}) {
+			defer wg.Done()
+			defer close(workOut)
+			f(workIn, workOut)
+		}(worker, in, out)
+		in = out
 	}
 	wg.Wait()
 }
@@ -124,6 +128,7 @@ func main() {
 		job(CombineResults),
 		job(func(in, out chan interface{}) {
 			dataRaw := <-in
+			fmt.Println(dataRaw)
 			data, _ := dataRaw.(string)
 			fmt.Println(data)
 		}),
